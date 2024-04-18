eb287253e079f409a464cf81265a4158c2f3ebad
Adriel Campos <adrielcampos.dev@gmail.com>
2024-04-18 20:51:51 -0300

    Inicializando projeto e estruturando card inicial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          alt="Rick Sanchez Image"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
